0b38cee0c14e254f62376dfbe709de7b8d5b7e16
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-04-15 12:02:13 +0200

    feat(ImageStore): add API

---
+++ src/api/ImageStore.js
@@ -0,0 +1,21 @@
+import ImageStoreManager from '../NativeModules/ImageStoreManager';
+
+class ImageStore {
+  static hasImageForTag(uri, callback) {
+    ImageStoreManager.hasImageForTag(uri, callback);
+  }
+
+  static removeImageForTag(uri) {
+    ImageStoreManager.removeImageForTag(uri);
+  }
+
+  static addImageFromBase64(base64ImageData, success, failure) {
+    ImageStoreManager.addImageFromBase64(base64ImageData, success, failure);
+  }
+
+  static getBase64ForTag(uri, success, failure) {
+    ImageStoreManager.getBase64ForTag(uri, success, failure);
+  }
+}
+
+module.exports = ImageStore;
