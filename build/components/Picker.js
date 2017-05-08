92a44f26d1a2c9aef651a7ad12e5c1443ce6d04b
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-05-08 14:59:10 +0200

    core: push build folder to allow npm install

---
+++ build/components/Picker.js
@@ -0,0 +1,17 @@
+var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);
+var _createReactClass=require('create-react-class');var _createReactClass2=_interopRequireDefault(_createReactClass);
+var _createMockComponent=require('./createMockComponent');var _createMockComponent2=_interopRequireDefault(_createMockComponent);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{'default':obj};}
+
+var Picker=(0,_createReactClass2['default'])({
+propTypes:{
+children:_propTypes2['default'].node},
+
+statics:{
+Item:(0,_createMockComponent2['default'])('Picker.Item')},
+
+render:function(){function render(){
+return null;
+}return render;}()});
+
+
+module.exports=Picker;
---
+++ build/components/TouchableNativeFeedback.js
@@ -0,0 +1,21 @@
+var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);
+var _createReactClass=require('create-react-class');var _createReactClass2=_interopRequireDefault(_createReactClass);
+var _TouchableWithoutFeedback=require('./TouchableWithoutFeedback');var _TouchableWithoutFeedback2=_interopRequireDefault(_TouchableWithoutFeedback);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{'default':obj};}
+
+var TouchableNativeFeedback=(0,_createReactClass2['default'])({
+propTypes:_extends({},
+_TouchableWithoutFeedback2['default'].propTypes,{
+
+background:_propTypes2['default'].object}),
+
+statics:{
+SelectableBackground:function(){function SelectableBackground(){}return SelectableBackground;}(),
+SelectableBackgroundBorderless:function(){function SelectableBackgroundBorderless(){}return SelectableBackgroundBorderless;}(),
+Ripple:function(){function Ripple(color,borderless){}return Ripple;}()},
+
+render:function(){function render(){
+return null;
+}return render;}()});
+
+
+module.exports=TouchableNativeFeedback;
